421a85411a653b9dbfa3da60c2ce1ccf7f5096ad
Monik Makwana <55778433+monikmakwana@users.noreply.github.com>
2020-10-01 10:09:30 +0530

    Add files via upload

---
+++ precache-manifest.c2c5a95b578a0e99547a1ec535a30daa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4539a233db5f41d9ef0ac4d9566db051",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8a7f53b91926bbba467",
+    "url": "/static/css/main.e2be2fb2.chunk.css"
+  },
+  {
+    "revision": "8765d7097ef1362ce912",
+    "url": "/static/js/2.78f62b35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78f62b35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8a7f53b91926bbba467",
+    "url": "/static/js/main.f386bc09.chunk.js"
+  },
+  {
+    "revision": "ffe148ba9c987666fc58",
+    "url": "/static/js/runtime-main.451dc41f.js"
+  }
+]);
